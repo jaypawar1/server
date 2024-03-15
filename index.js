42dc2fcf2bce9e2aa4af1b2034bf4ce0568d772f
Pranav Hole <85498337+pranavhole@users.noreply.github.com>
2024-03-15 11:36:46 +0530

    first commit

--- index.js
+++ index.js
@@ -1,54 +1,62 @@
 const express = require("express");
-const body_parser = require('body-parser');
+const bodyParser = require('body-parser');
+const axios = require('axios');
 
-const app = express().use(body_parser.json());
+const app = express().use(bodyParser.json());
 
 app.listen(8000, () => {
-    console.log("webhook is listning");
-})
+    console.log("webhook is listening");
+});
+
 const token = "EAAIqSsKeP0QBOZCXPRe2U4DKxdIXEBnSKpT6D1kWdbCG63cYZASyo1WjbdPnoFO7dfNsv7muvfwdiaa6FwlrnHgI63jxcrJnqnFHK0rVxnZAe7uo2KmSwxl23GfBggCJi3rOrkdaYFDdwWyl76rCq1ZAT9Dwl5PXFfnM1rSyYX9MsEjDKaOqJMVavj1PxKO6Xt0SrBmDZAaTRuuesSWcZD";
-const mytoken = "sdhhijsfh"
+const myToken = "sdhhijsfh";
+
 app.get("/webhook", (req, res) => {
     let mode = req.query["hub.mode"];
-    let chalange = req.query["hub.challenge"];
+    let challenge = req.query["hub.challenge"];
     let token = req.query["hub.verify_token"];
-    if (mode && chalange) {
-        if (mode === "subscribe" && token === mytoken) {
-            console.log(chalange);
-            res.status(200).send(chalange);
+    if (mode && challenge) {
+        if (mode === "subscribe" && token === myToken) {
+            console.log(challenge);
+            res.status(200).send(challenge);
         } else {
             res.sendStatus(403);
         }
     }
-})
+});
 
 app.post("/webhook", (req, res) => {
-    let body_param = req.body;
+    let bodyParam = req.body;
 
-    console.log(JSON.stringify(body_param, null, 2));
+    console.log(JSON.stringify(bodyParam, null, 2));
 
-    if (body_param.object) {
-        if (body_param.entry &&
-            body_param.entry[0].changes &&
-            body_param.entry[0].changes[0].value.message &&
-            body_param.entry[0].changes[0].value.message[0]) {
-            let phone_no_id = body_param.entry[0].challange[0].value.metadata.phone_number_id;
-            let from = body_param.entry[0].changes[0].value.messages[0].from;
-            let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+    if (bodyParam.object === "page") {
+        if (bodyParam.entry &&
+            bodyParam.entry[0].changes &&
+            bodyParam.entry[0].changes[0].value.message &&
+            bodyParam.entry[0].changes[0].value.message[0]) {
+            let phoneNumberId = bodyParam.entry[0].changes[0].value.metadata.phone_number_id;
+            let from = bodyParam.entry[0].changes[0].value.message[0].from;
+            let msgBody = bodyParam.entry[0].changes[0].value.message[0].text;
             axios({
                 method: "POST",
-                url: "https://graph.facebook.com/v13.0/" + phon_no_id + "/message?access_token=" + token,
+                url: "https://graph.facebook.com/v13.0/" + phoneNumberId + "/message?access_token=" + token,
                 data: {
-                    messaging_product: "whatsapp",
-                    to: from,
-                    text: {
-                        body: "Hi.. I'm Prasath"
+                    messaging_type: "RESPONSE",
+                    message: {
+                        text: "Hi.. I'm Prasath"
                     }
                 },
                 headers: {
                     "Content-Type": "application/json"
                 }
+            }).then(response => {
+                console.log("Message sent successfully:", response.data);
+            }).catch(error => {
+                console.error("Error sending message:", error);
             });
         }
     }
-})
+    res.status(200).send("EVENT_RECEIVED");
+});
+
